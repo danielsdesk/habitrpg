f86ae535e10b9237e3f19b19cea3e83ae0fdbf57
Sabe Jones <sabrecat@gmail.com>
2015-07-10 15:03:20 -0500

    feat(analytics): Heap

--- website/public/js/services/analyticsServices.js
+++ website/public/js/services/analyticsServices.js
@@ -32,6 +32,19 @@
       }, window['ga'].l = 1 * new Date();
     ga('create', window.env.GA_ID, user ? {'userId': user._id} : undefined);
 
+    window.heap = window.heap || [], heap.load = function(t, e) {
+      window.heap.appid = t, window.heap.config = e;
+      var a = document.createElement("script");
+      a.type = "text/javascript", a.async = !0, a.src = ("https:" === document.location.protocol ? "https:" : "http:") + "//cdn.heapanalytics.com/js/heap-" + t + ".js";
+      var n = document.getElementsByTagName("script")[0];
+      n.parentNode.insertBefore(a, n);
+      for (var o = function(t) {
+        return function() {
+          heap.push([t].concat(Array.prototype.slice.call(arguments, 0)))
+        }
+      }, p = ["clearEventProperties", "identify", "setEventProperties", "track", "unsetEventProperty"], c = 0; c < p.length; c++) heap[p[c]] = o(p[c])
+    };
+
     function loadScripts() {
       setTimeout(function() {
         // Amplitude
